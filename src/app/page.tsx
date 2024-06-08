73aaf05edefee70801465e310a2cb0db5c992825
dev-abuhasan <ahinstitutte@gmail.com>
2024-06-08 23:03:23 +0600

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="flex justify-center flex-col items-start min-h-screen w-full bg-red-100">
+      <h1 className="text-bolder w-full text-black text-3xl text-center">DHSC 2k17</h1>
+      <h1 className="text-bolder w-full text-black text-2xl text-center">Something is cooking....</h1>
+    </div>
+  );
+};
+
+export default Home;
